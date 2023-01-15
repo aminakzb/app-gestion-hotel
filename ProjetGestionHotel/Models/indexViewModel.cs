61e58321bf3a77979e3397e26b1d99252308530c
AMINA KZIBER <aminaaakz123@gmail.com>
2023-01-15 22:48:36 +0100

    first commit

---
+++ ProjetGestionHotel/Models/indexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjetGestionHotel.Models
+{
+    public class indexViewModel
+    {
+        public  List<administrateur> administrateurModelObject { get; set; }
+        public List<client> clientModelObject { get; set; }
+        public List<reservation> reservationModelObject { get; set; }
+        public List<commentaire> commentaireModelObject { get; set; }
+    }
+}
